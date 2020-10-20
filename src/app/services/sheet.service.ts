d3bb1138960e2d2f9c6f8029e6f87efeda2ef0d4
yuanyenan <123456>
2020-10-20 17:25:50 +0800

    歌单

--- src/app/services/sheet.service.ts
+++ src/app/services/sheet.service.ts
@@ -1,21 +1,29 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/internal/operators';
-import { SongSheet } from './data-types/common.type';
+import { map, pluck, switchMap } from 'rxjs/internal/operators';
+import { Song, SongSheet } from './data-types/common.type';
 import { API_CONFIG, ServicesModule } from './services.module';
+import { SongService } from './song.service';
 
 @Injectable({
   providedIn: ServicesModule
 })
 export class SheetService {
 
-  constructor(private httpClient:HttpClient , @Inject(API_CONFIG) private url:string ) { }
+  constructor(private httpClient:HttpClient ,private songService:SongService , @Inject(API_CONFIG) private url:string ) { }
 
   //获取歌单详情
   getSongSheetDetail(id:number):Observable<SongSheet>{
     const params = new HttpParams().set('id',id.toString());
-    return this.httpClient.get(this.url+"playlist/detail",{params}).pipe(map((res:{playlist:SongSheet})=>res.playlist));
+    return this.httpClient.get(this.url+"playlist/detail",{params})
+    .pipe(map((res:{playlist:SongSheet})=>res.playlist));
+
+  }
+
+
+  playSheet(id:number):Observable<Song[]>{
+    return this.getSongSheetDetail(id).pipe(pluck('tracks'),switchMap(tracks=>this.songService.getSongList(tracks)));
 
   }
 
