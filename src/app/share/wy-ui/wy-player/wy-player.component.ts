db88bdedae09d238e97c9f129d39afbe62e45a5f
yynlove <1993208827@qq.com>
2020-11-01 21:19:05 +0800

    播放暂停 上一曲 下一曲

--- src/app/share/wy-ui/wy-player/wy-player.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Song } from 'src/app/services/data-types/common.type';
 import { AppStoreModule } from 'src/app/store';
+import { SetCurrentIndex } from 'src/app/store/actions/palyer-action';
 import { getCurrentIndex, getCurrentSong, getPlayer, getPlayList, getPlayMode, getSongList } from 'src/app/store/selectors/player.selector';
 import { PlayMOde } from './player-type';
 
@@ -24,6 +25,12 @@ export class WyPlayerComponent implements OnInit {
   duration:number;
   //歌曲当前播放的时间
   currentTime: number;
+
+  //播放状态
+  playing = false;
+  //是否可以播放
+  songReady = false;
+
 
   @ViewChild('audio',{ static : true }) private audio :ElementRef;
   private audioEl : HTMLAudioElement;
@@ -60,7 +67,10 @@ export class WyPlayerComponent implements OnInit {
     this.audioEl = this.audio.nativeElement;
   }
 
-  //绑定数据
+  /**
+   *
+   * 绑定数据
+   */
   private wactchList(list: Song[],type:string){
     console.log("player-"+type+">>>",list);
     this[type] = list;
@@ -81,13 +91,68 @@ export class WyPlayerComponent implements OnInit {
   }
 
 
+  /**
+   * 播放/暂停
+   */
+  onToggle(){
+    if(!this.currentSong){
+      if(this.playList.length){
+        //如果有歌曲 没有播放
+        this.updateIndex(0);
+      }
+    }else{
+      if(this.songReady){
+        this.playing = !this.playing;
+        if(this.playing){
+          this.audioEl.play();
+        }else{
+          this.audioEl.pause();
+        }
+      }
+    }
+  }
 
-  //当歌曲可以播放的时候就进行播放
-  onCanplay(){
+
+
+  onPrev(index: number){
+    if(!this.songReady) return;
+    if(this.playList.length ===1){
+      this.loop();
+    }else{
+      const newIndex = index <=0 ? this.playList.length -1 :index;
+      this.updateIndex(newIndex);
+    }
+
+  }
+
+  onNext(index : number){
+    if(!this.songReady) return;
+    if(this.playList.length ===1){
+      this.loop();
+    }else{
+      const newIndex = index >= this.playList.length ? 0:index;
+      this.updateIndex(newIndex);
+    }
+  }
+  loop() {
+   this.audioEl.currentTime = 0;
+   this.play();
+  }
+
+
+
+
+  /**
+   *
+   * 当歌曲可以播放的时候就进行播放
+   */
+   onCanplay(){
+    this.songReady = true;
     this.play();
   }
   play() {
     this.audioEl.play();
+    this.playing=true;
   }
 
 
@@ -101,5 +166,12 @@ export class WyPlayerComponent implements OnInit {
   //歌曲播放监听 当前时间
   onTimeUpdate(e:Event){
     this.currentTime = (<HTMLAudioElement>e.target).currentTime;
+  }
+
+
+
+  updateIndex(index: number){
+    this.store$.dispatch(SetCurrentIndex({ currentIndex : index}));
+    this.songReady = false;
   }
 }
